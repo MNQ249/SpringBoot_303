7035af6d86fb7169d0881edb28705174848ecefc
Mesfer <mesfer.vip@gmail.com>
2019-12-27 08:45:35 +0300

    implementing function to autoload data on application start

--- src/main/java/com/example/demo/DataLoader.java
+++ src/main/java/com/example/demo/DataLoader.java
@@ -1,4 +1,25 @@
 package com.example.demo;
 
-public class DataLoader {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    @Autowired
+    CourseRepository repository;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        Course course;
+        course = new Course("Astrophsics", "Neil D Tyson", "Just a course on stars", 3);
+        repository.save(course);
+
+        course = new Course("Calculus", "Carol Henley", "Rate of Change of the Rate of Change",3);
+        repository.save(course);
+
+        course = new Course("Freshman English", "Geraldin pegram ", "Learn your Language childern", 3);
+        repository.save(course);
+    }
 }
